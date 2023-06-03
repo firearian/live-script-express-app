285fa2bbfc5215616296311f3c622177238df7a0
Azul Alysum <Azul.Alysum@ibm.com>
2023-06-04 02:56:19 +1200

    Basic Implementation

--- server/server.js
+++ server/server.js
@@ -1,25 +1,66 @@
 // import dependencies and initialize express
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
+import expressWebsockets from "express-ws";
+import { Server } from "@hocuspocus/server";
 
-const healthRoutes = require("./routes/health-route");
-const swaggerRoutes = require("./routes/swagger-route");
+import express from "express";
 
-const app = express();
+// Configure Hocuspocus
+const server = Server.configure({
+  async connected() {
+    console.log("connections:", server.getConnectionsCount());
+  },
+});
+server.listen();
 
-// enable parsing of http request body
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// Setup your express instance using the express-ws extension
+const { app } = expressWebsockets(express());
 
-// routes and api calls
-app.use("/health", healthRoutes);
-app.use("/swagger", swaggerRoutes);
+// Add a websocket route for Hocuspocus
+// Note: make sure to include a parameter for the document name.
+// You can set any contextual data like in the onConnect hook
+// and pass it to the handleConnection method.
+app.ws("/collaboration/:document", (websocket, request) => {
+  console.log("ws entered?");
+  const context = {
+    user: {
+      id: 1234,
+      name: "Jane",
+    },
+  };
 
-// default path to serve up index.html (single page application)
-// app.all("", (req, res) => {
-//   res.status(200).sendFile(path.join(__dirname, "../public", "index.html"));
-// });
+  server.handleConnection(websocket, request, context);
+});
+
+app.ws("status", (websocket, request) => {
+  const context = {
+    user: {
+      id: 1234,
+      name: "Jane",
+    },
+  };
+
+  server.handleConnection(websocket, request, context);
+});
+// const path = require("path");
+// const bodyParser = require("body-parser");
+
+// const healthRoutes = require("./routes/health-route");
+// const swaggerRoutes = require("./routes/swagger-route");
+
+// const app = express();
+
+// // enable parsing of http request body
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// // routes and api calls
+// app.use("/health", healthRoutes);
+// app.use("/swagger", swaggerRoutes);
+
+// default path
+app.all("", (req, res) => {
+  res.status(200).send("Hello World");
+});
 
 // start node server
 const port = process.env.PORT || 4000;
@@ -32,5 +73,3 @@ app.listen(port, () => {
 // app.use((req, res, next) => {
 //   res.sendFile(path.join(__dirname, "../public", "404.html"));
 // });
-
-module.exports = app;
