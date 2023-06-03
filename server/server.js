b889ec1c96432a2c2b8bc459f3fd3dbaddf60bfb
Azul Alysum <Azul.Alysum@ibm.com>
2023-06-04 02:13:51 +1200

    Remove Uneccesary

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 // import dependencies and initialize express
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
 
-const healthRoutes = require('./routes/health-route');
-const swaggerRoutes = require('./routes/swagger-route');
+const healthRoutes = require("./routes/health-route");
+const swaggerRoutes = require("./routes/swagger-route");
 
 const app = express();
 
@@ -13,24 +13,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // routes and api calls
-app.use('/health', healthRoutes);
-app.use('/swagger', swaggerRoutes);
+app.use("/health", healthRoutes);
+app.use("/swagger", swaggerRoutes);
 
 // default path to serve up index.html (single page application)
-app.all('', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, '../public', 'index.html'));
-});
+// app.all("", (req, res) => {
+//   res.status(200).sendFile(path.join(__dirname, "../public", "index.html"));
+// });
 
 // start node server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`App UI available http://localhost:${port}`);
   console.log(`Swagger UI available http://localhost:${port}/swagger/api-docs`);
 });
 
 // error handler for unmatched routes or api calls
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, '../public', '404.html'));
-});
+// app.use((req, res, next) => {
+//   res.sendFile(path.join(__dirname, "../public", "404.html"));
+// });
 
 module.exports = app;
