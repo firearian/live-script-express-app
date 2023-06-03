ebffdf7c57d6c8b36aa41e5ab109f042051d46b6
Azul Alysum <Azul.Alysum@ibm.com>
2023-06-04 05:25:15 +1200

    c

--- server/server.js
+++ server/server.js
@@ -8,7 +8,42 @@ const bodyParser = require("body-parser");
 const healthRoutes = require("./routes/health-route");
 const swaggerRoutes = require("./routes/swagger-route");
 
-const app = express();
+// Configure Hocuspocus
+const server = Server.configure({
+  async connected() {
+    console.log("connections:", server.getConnectionsCount());
+  },
+});
+
+// Setup your express instance using the express-ws extension
+const { app } = expressWebsockets(express());
+
+// Add a websocket route for Hocuspocus
+// Note: make sure to include a parameter for the document name.
+// You can set any contextual data like in the onConnect hook
+// and pass it to the handleConnection method.
+app.ws("/collaboration/:document", (websocket, request) => {
+  console.log("ws entered?");
+  const context = {
+    user: {
+      id: 1234,
+      name: "Jane",
+    },
+  };
+
+  server.handleConnection(websocket, request, context);
+});
+
+app.ws("status", (websocket, request) => {
+  const context = {
+    user: {
+      id: 1234,
+      name: "Jane",
+    },
+  };
+
+  server.handleConnection(websocket, request, context);
+});
 
 // enable parsing of http request body
 app.use(bodyParser.urlencoded({ extended: false }));
