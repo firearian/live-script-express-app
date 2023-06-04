7dee31423b819d6fd1fe900d2486118b7cd7c634
Azul Alysum <Azul.Alysum@ibm.com>
2023-06-04 23:29:48 +1200

    Deploy commit

--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
-// import dependencies and initialize express
 const expressWebsockets = require("express-ws");
 const { Server } = require("@hocuspocus/server");
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-
 const healthRoutes = require("./routes/health-route");
 const swaggerRoutes = require("./routes/swagger-route");
 
@@ -47,27 +45,3 @@ app.ws("status", (websocket, request) => {
 
 // enable parsing of http request body
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// routes and api calls
-app.use("/health", healthRoutes);
-app.use("/swagger", swaggerRoutes);
-
-// default path to serve up index.html (single page application)
-app.all("", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "../public", "index.html"));
-});
-
-// start node server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`App UI available http://localhost:${port}`);
-  console.log(`Swagger UI available http://localhost:${port}/swagger/api-docs`);
-});
-
-// error handler for unmatched routes or api calls
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "../public", "404.html"));
-});
-
-module.exports = app;
